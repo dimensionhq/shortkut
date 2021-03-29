739aa7811f50bac2917bad7952be441d3e68b1d0
XtremeDevX <xtremedevx@gmail.com>
2021-03-29 10:24:08 +0530

    Updated cargo.json and Added get() method

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,48 @@ mod utils;
 
 use colored::*;
 use std::env;
-use utils::*;
+use std::time::Instant;
 
-const __version__: &str = "1.0.0";
+const __VERSION__: &str = "1.0.0";
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let start = Instant::now();
 
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
     let command: &str = &args[1];
 
     match command {
         "add" => {
-            println!("shc {} {}", __version__, "add".green().bold());
+            println!("shc {} {}", __VERSION__, "add".green().bold());
 
             // 2 Possibilities : shc add cargo and shc add cru < cargo run
+            // TODO: Handle Multiple Shortcuts shc add cargo,git
 
-            utils::get()
+            if args.len() == 3 {
+                let response = utils::get_shortcut(&args[2]);
+                println!(
+                    "Adding {}",
+                    &response["display-name"].to_string().bright_cyan()
+                );
+            } else {
+            }
         }
         "remove" => {
-            println!("shc {} {}", __version__, "remove".green().bold());
+            println!("shc {} {}", __VERSION__, "remove".green().bold());
         }
         _ => println!("Invalid Command!"),
     }
     // get()
 
-    // write, i don't write
     // then we need to create shortcuts
 
     // then we need to print success
+    let end = Instant::now();
+    println!(
+        "Added {} {} in {:.2}s",
+        1.to_string().bright_green(),
+        "shortcut",
+        (end - start).as_secs_f32()
+    );
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,57 @@
+use colored::*;
 use reqwest::blocking;
+use serde_json::Value;
+use std::process;
 
-pub fn get_shortcut(name: &str) {
-    blocking::get("https://google.com");
+pub fn get_shortcut(name: &str) -> Value {
+    let mut res: String = String::new();
+
+    match blocking::get(format!(
+        "https://raw.githubusercontent.com/XtremeDevX/shc/master/shortcuts/{}.json",
+        name
+    )) {
+        Ok(response) => {
+            if response.status() == reqwest::StatusCode::OK {
+                // Response Code Is 200 OK
+                match response.text() {
+                    Ok(text) => {
+                        res = text;
+                    }
+                    Err(e) => {
+                        println!(
+                            "{} {}",
+                            "Failed To Parse Response:".to_string().yellow(),
+                            e.to_string().bright_red().bold()
+                        );
+                        process::exit(1);
+                    }
+                }
+            } else {
+                println!(
+                    "{} is not a valid shortcut pack.",
+                    name.to_string().bright_magenta().bold()
+                );
+                process::exit(1);
+            }
+        }
+        Err(err) => println!("Failed To Request {}.json => {}", name, err),
+    }
+
+    let data: Value;
+
+    match serde_json::from_str(&res) {
+        Ok(json) => {
+            data = json;
+        }
+        Err(err) => {
+            println!(
+                "Failed to parse {}.json, {}",
+                name.to_string().bright_magenta().bold(),
+                err.to_string().bright_red().bold(),
+            );
+            process::exit(1);
+        }
+    };
+
+    data
 }
